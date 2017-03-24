9a80d788f02fbb631e99daa743a7d88b0d7d4c77
Evan Busse <evanbusse@gmail.com>
2017-03-24 16:37:19 -0600

    added navbar components linking correctly

---
+++ src/components/navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const NavBar = React.createClass({
+  getInitialState() {
+    return null;
+  },
+
+  render() {
+    return (
+      <div>
+        hello again
+      </div>
+    )
+  }
+})
+
+export default NavBar;
---
+++ src/views/landing.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import NavBar from '../components/navbar'
+
+const Landing = React.createClass({
+  render() {
+    return (
+      <div>
+        <NavBar/>
+        hello world
+      </div>
+    )
+  }
+})
+
+export default Landing;
